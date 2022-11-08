7a3c9f51e4ccee448ff1b9351b27185f85352e04
Denis <Mrgauthier34@gmail.com>
2022-11-07 22:29:07 -0800

    Okay I have fixed your function & made sure it works with the sorting now!

--- notebooks/project_function2.py
+++ notebooks/project_function2.py
@@ -14,29 +14,23 @@ def load_and_process(file):
         .drop("DATA_LEVEL", axis = 1)
         .drop("SUB_POPULATION", axis = 1)
         .drop("COMPLETION_RATE_MODEL", axis = 1)
+        .rename(columns = {"DISTRICT_NAME":"School District"})
+        .rename(columns = {"YEAR_6_OF_COHORT":"Year"})
+        .rename(columns = {"ESTIMATED_COMPLETION_RATE":"Estimated Completion Rate"})
+        .sort_values(by=["School District"], ascending=True)
     )
-    df2 = (
-        df1
 #Rename columns
-        .rename(columns = {"DISTRICT_NAME" : "School District"})
-        .rename(columns =  {"YEAR_6_OF_COHORT" : "Year"})
-        .rename(columns =  {"ESTIMATED_COMPLETION_RATE" : "Estimated Completion Rate"})
- 
 #Sort values    
         #.sort_values(by=["DISTRICT_NAME"], ascending=True)
            
-    )
 #Delete unneeded values
-    #df2 = df2[df2['DATA_LEVEL'].str.contains('DISTRICT LEVEL')==True]
-    #df2 = df2[df2['SUB_POPULATION'].str.contains('ALL STUDENTS')==True]
-    df2 = df2[df2['School District'].str.contains('NaN')==False]
-    #df2 = df2[df2['COMPLETION_RATE_MODEL'].str.contains('SIX YEAR DOGWOOD COMPLETION RATE')==True]
-    df2 = df2[df2['Estimated Completion Rate'].str.contains('Msk')==False]
+    df1 = df1[df1['School District'].str.contains('NaN')==False]
+    df1 = df1[df1['Estimated Completion Rate'].str.contains('Msk')==False]
 
 #Shorten year column   
-    df2 = df2["Year"].str.slice(0,-5) 
+    df1['Year'] = df1["Year"].str.slice(0,-5) 
 
 #Change year column to int
-    df2 = df2["Year"].astype(int)         
+    df1['Year'] = df1["Year"].astype(int)         
 
-    return df2 
+    return df1 
